870aa2e885640908d83d333a13ead98de38c0d8e
Rafael <rafael.waltrick@fatec.sp.gov.br>
2021-11-30 21:31:04 -0300

    make it better

---
+++ src/app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/projecao")
+def projecao():
+    return render_template("projecao.html")
+@app.route("/sobremim")
+def sobremim():
+    return render_template("sobremim.html")
+@app.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")    
